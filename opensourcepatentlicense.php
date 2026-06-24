c1195e1c5301fcf5a8aee0712065328b1e7909e2
Faceworld123 <chrisclark777cmc@gmail.com>
2026-06-24 14:41:36 -0500

    Add files via upload

---
+++ opensourcepatentlicense.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="robots" content="all" /> 
+<meta name="robots" content="index, follow" /> 
+<meta name="googlebot" content="index, follow" /> 
+<meta name="msnbot" content="index, follow" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Faceworld | About</title>
+<link href="http://10.1.10.223:8080/css/opensourcepatentlicense.css" rel="stylesheet" type="text/css" media="screen" />
+<meta name="description" content="Faceworld is an utility that connects you to friends and family by location!">
+<meta name="keywords" content="social, social network, network" />
+<link rel="shortcut icon" href="http://10.1.10.223:8080/img/favicon.ico">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+</head>
+
+<body>
+<div id="wrapper">
+
+  <div id="header">
+    <div id="center_box">
+      <div id="rightbox"><strong><a class="white" href="http://10.1.10.223:8080/index.php">Faceworld</a></strong></div>
+    </div>
+  </div>
+  <div id="cube">
+    
+    <div id="rect_box">
+      <div id="subjectbx"><strong>Open source patent license</strong></div>
+      <div id="center_txt">
+        <p> <h1> <a class="white" href="http://10.1.10.223:8080/opensourcepatentlicense.txt">Open source patent license</a></h1>
+</p>
+</div></div>
+     
+<div id="footer">
+  <div id="bx1"><strong><a class="gray" href="http://10.1.10.223:8080/terms.php">Terms</a> | <a class="gray" href="http://10.1.10.223:8080/privacy.php">Privacy</a> | <a class="gray" href="http://10.1.10.223:8080/index.php">&copy; 2026 Faceworld</a></strong></div>
+  <div id="bx2"></div>
+</div>
+  </div>
+</div>
+
+</body>
+
+</html>
+
+
+
+<?php include_once("analyticstracking.php") ?>
